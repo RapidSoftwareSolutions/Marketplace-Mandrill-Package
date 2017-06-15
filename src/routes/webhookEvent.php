c46ddc027752c5973939da2db7cd25e23de94364
Stefan Skliarov <serg.osipchuk@rapidapi.com>
2017-06-15 15:22:53 +0300

    webhook checking3

--- src/routes/webhookEvent.php
+++ src/routes/webhookEvent.php
@@ -9,10 +9,11 @@ $app->post('/api/Mandrill/webhookEvent', function ($request, $response, $args) {
         $post_data = $validateRes;
     }
 
+
     $result = [
         "http_resp" => "",
-        "client_msg" => $post_data['args'],
-        "params" => []
+        "client_msg" => $post_data['args']['body'],
+        "params" => $post_data['args']['params']
     ];
     $client = new GuzzleHttp\Client();
     try {
