b7bb6d2c44bb0b7b8450a4c1619dd31b3b58957b
Stefan Skliarov <serg.osipchuk@rapidapi.com>
2017-06-15 15:36:23 +0300

    webhook checking4

--- src/routes/webhookEvent.php
+++ src/routes/webhookEvent.php
@@ -15,43 +15,7 @@ $app->post('/api/Mandrill/webhookEvent', function ($request, $response, $args) {
         "client_msg" => $post_data['args']['body'],
         "params" => $post_data['args']['params']
     ];
-    $client = new GuzzleHttp\Client();
-    try {
-        $resp = $client->request('POST', 'http://d7c2294c.ngrok.io', [
-            'json' => $result
-        ]);
-
-        $responseBody = $resp->getBody()->getContents();
-        $rawBody = json_decode($resp->getBody());
-
-        $all_data[] = $rawBody;
-        if ($response->getStatusCode() == '200') {
-            $result['callback'] = 'success';
-            $result['contextWrites']['to'] = is_array($all_data) ? $all_data : json_decode($all_data);
-        } else {
-            $result['callback'] = 'error';
-            $result['contextWrites']['to']['status_code'] = 'API_ERROR';
-            $result['contextWrites']['to']['status_msg'] = is_array($responseBody) ? $responseBody : json_decode($responseBody);
-        }
-    } catch (\GuzzleHttp\Exception\ClientException $exception) {
-        $responseBody = $exception->getResponse()->getReasonPhrase();
-        $result['callback'] = 'error';
-        $result['contextWrites']['to']['status_code'] = 'API_ERROR';
-        $result['contextWrites']['to']['status_msg'] = $responseBody;
-
-    } catch (GuzzleHttp\Exception\ServerException $exception) {
-
-        $responseBody = $exception->getResponse()->getBody(true);
-        $result['callback'] = 'error';
-        $result['contextWrites']['to'] = json_decode($responseBody);
-
-    } catch (GuzzleHttp\Exception\BadResponseException $exception) {
-
-        $responseBody = $exception->getResponse()->getBody(true);
-        $result['callback'] = 'error';
-        $result['contextWrites']['to'] = json_decode($responseBody);
-
-    }
+   
     $result['callback'] = 'success';
     $result['contextWrites']['to'] = $result;
 
